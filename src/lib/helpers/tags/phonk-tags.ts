226018cb0ebc6e4c4f5c8ccaf9f1961e631a056c
Nicholas <contactnicholas81@gmail.com>
2025-05-07 13:22:58 +0100

    update phonk tags

--- src/lib/helpers/tags/phonk-tags.ts
+++ src/lib/helpers/tags/phonk-tags.ts
@@ -1,3 +1,3 @@
 export const phonkTags = (artist: string, title: string): string => {
-  return `${artist},${title},${artist} ${title},${title} ${artist},${title} phonk,${artist} ${title} phonk,${title} ${artist} phonk,${artist} phonk`;
+  return `${artist},${title},${artist} ${title},${title} ${artist},${title} phonk,${artist} ${title} phonk,${title} ${artist} phonk,${artist} phonk,brazilian phonk,funk,new phonk,tiktok phonk,hard phonk,${title} funk,${artist} funk,${artist} ${title} funk,phonk ${new Date().getFullYear()}`;
 };
