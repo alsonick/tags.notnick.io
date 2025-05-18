6708077305e9e779fda80c163f7621e837c7775b
Nicholas <contactnicholas81@gmail.com>
2025-05-18 19:13:47 +0100

    add analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,15 @@
-import "@/styles/globals.css";
+import { Analytics } from "@vercel/analytics/next";
 import type { AppProps } from "next/app";
 import { Inter } from "next/font/google";
+import "@/styles/globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <main className={inter.className}><Component {...pageProps} /></main>;
+  return (
+    <main className={inter.className}>
+      <Component {...pageProps} />
+      <Analytics />
+    </main>
+  );
 }
