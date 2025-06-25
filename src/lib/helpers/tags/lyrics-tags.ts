388296e5f970c1d9c0c7307c06afe9955b46673e
Nicholas <contactnicholas81@gmail.com>
2025-06-25 15:28:50 +0100

    fix tags

--- src/lib/helpers/tags/lyrics-tags.ts
+++ src/lib/helpers/tags/lyrics-tags.ts
@@ -10,18 +10,18 @@ export const lyricsTags = (artist: string, title: string, features: string, tikt
 
   // Features
   if (features !== "none" && (tiktok === "false" || tiktok === "" || tiktok !== "true")) {
-    tags += `,${firstFeature},${firstFeature} ${title} lyrics,lyrics ${firstFeature} ${title},${firstFeature} lyrics`;
+    tags += `,${firstFeature},${firstFeature} ${title} lyrics,lyrics ${firstFeature} ${title},${firstFeature} lyrics,${firstFeature} ${title},${artist} ${firstFeature},${title} ${firstFeature}`;
 
     // Second feature
     if (feats.length === 2) {
       const secondFeature = feats[1];
-      tags += `,${secondFeature},${secondFeature} ${title} lyrics`;
+      tags += `,${secondFeature},${secondFeature} ${title} lyrics,${secondFeature} ${title},${artist} ${secondFeature},${title} ${secondFeature}`;
     }
 
     // Third feature
     if (feats.length === 3) {
       const thirdFeature = feats[2];
-      tags += `,${thirdFeature},${thirdFeature} ${title} lyrics`;
+      tags += `,${thirdFeature},${thirdFeature} ${title} lyrics,${thirdFeature} ${title},${artist} ${thirdFeature},${title} ${thirdFeature}`;
     }
   }
 
