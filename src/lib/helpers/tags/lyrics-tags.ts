8f4d4bcbba081f2b6218a7dcfdeffc39a6e78bf4
Nicholas <contactnicholas81@gmail.com>
2025-07-18 09:51:53 +0100

    revert

--- src/lib/helpers/tags/lyrics-tags.ts
+++ src/lib/helpers/tags/lyrics-tags.ts
@@ -13,13 +13,13 @@ export const lyricsTags = (artist: string, title: string, features: string, tikt
     tags += `,${firstFeature},${firstFeature} ${title} lyrics,lyrics ${firstFeature} ${title},${firstFeature} lyrics,${firstFeature} ${title},${artist} ${firstFeature},${title} ${firstFeature}`;
 
     // Second feature
-    if (feats.length === 1) {
+    if (feats.length === 2) {
       const secondFeature = feats[1];
       tags += `,${secondFeature},${secondFeature} ${title} lyrics,${secondFeature} ${title},${artist} ${secondFeature},${title} ${secondFeature}`;
     }
 
     // Third feature
-    if (feats.length === 2) {
+    if (feats.length === 3) {
       const thirdFeature = feats[2];
       tags += `,${thirdFeature},${thirdFeature} ${title} lyrics,${thirdFeature} ${title},${artist} ${thirdFeature},${title} ${thirdFeature}`;
     }
