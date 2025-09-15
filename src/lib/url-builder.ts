77faaba19b4fa00f9e654677c6757b150ec499c9
Nicholas <contactnicholas81@gmail.com>
2025-09-15 19:20:47 +0100

    minor change

--- src/lib/url-builder.ts
+++ src/lib/url-builder.ts
@@ -14,13 +14,15 @@ export const urlBuilder = (
   title: string,
   log: string
 ): string => {
-  return `/api/generate?title=${encodeURIComponent(customFormat.length ? "none" : title)}&artist=${encodeURIComponent(
-    customFormat.length ? artist.trim() : artist
-  )}&features=${encodeURIComponent(features)}&tiktok=${tiktok === "true"}&format=${encodeURIComponent(
-    format
-  )}&channel=${channel ? encodeURIComponent(channel) : "none"}&shuffle=${shuffle === "true"}&genre=${encodeURIComponent(
-    genre.toLowerCase()
-  )}&verse=${encodeURIComponent(verse.toLowerCase())}&custom=${customFormat ? "true" : "false"}&log=${
+  return `/api/v1/generate?title=${encodeURIComponent(
+    customFormat.length ? "none" : title
+  )}&artist=${encodeURIComponent(customFormat.length ? artist.trim() : artist)}&features=${encodeURIComponent(
+    features
+  )}&tiktok=${tiktok === "true"}&format=${encodeURIComponent(format)}&channel=${
+    channel ? encodeURIComponent(channel) : "none"
+  }&shuffle=${shuffle === "true"}&genre=${encodeURIComponent(genre.toLowerCase())}&verse=${encodeURIComponent(
+    verse.toLowerCase()
+  )}&custom=${customFormat ? "true" : "false"}&log=${
     log.toLowerCase() === "true" ? "true" : "false"
   }&response=${responseId}&example=${example}&source=${source}`;
 };
