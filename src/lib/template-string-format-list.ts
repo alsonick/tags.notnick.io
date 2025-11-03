52408cf5cd850892ded494a497f4ef8232154456
Nicholas <contactnicholas81@gmail.com>
2025-11-03 15:05:23 +0000

    add tiktok tags for lyrics

--- src/lib/template-string-format-list.ts
+++ src/lib/template-string-format-list.ts
@@ -49,6 +49,11 @@ export const TEMPLATE_STRING_FORMAT_LIST: TemplateStringFormatList[] = [
         template:
           "{artist} {title},{artist} {title} lyrics,{title} lyrics,{title} {artist} lyrics,lyrics {title},{artist} lyrics {title},{title} lyric video,{artist} lyrics,lyrics {artist},{title},{artist},{title} {artist},lyrics {title} {artist},lyrics {artist} {title},lyrics,{firstFeature},{firstFeature} {title} lyrics,lyrics {firstFeature} {title},{firstFeature} lyrics,{firstFeature} {title},{artist} {firstFeature},{title} {firstFeature},{secondFeature},{secondFeature} {title} lyrics,{secondFeature} {title},{artist} {secondFeature},{title} {secondFeature},{thirdFeature},{thirdFeature} {title} lyrics,{thirdFeature} {title},{artist} {thirdFeature},{title} {thirdFeature}",
       },
+      {
+        id: generateRandomId(),
+        constraint: "(lyrics)::[@tiktok=true@]",
+        template: "tiktok,{title} tiktok,trending tiktok,tiktok songs",
+      },
     ],
   },
 ];
