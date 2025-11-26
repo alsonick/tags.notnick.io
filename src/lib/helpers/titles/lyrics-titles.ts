c52dc6d14f1cbc014a0f1889ccbf0fda8767cf0e
Nicholas <contactnicholas81@gmail.com>
2025-11-26 12:27:39 +0000

    vibe music

--- src/lib/helpers/titles/lyrics-titles.ts
+++ src/lib/helpers/titles/lyrics-titles.ts
@@ -1,30 +1,22 @@
-export const lyricsTitles = (
-  artist: string,
-  title: string,
-  feats: string[]
-): string => {
+export const lyricsTitles = (artist: string, title: string, feats: string[]): string => {
   switch (feats.length) {
     case 1:
       return `${artist.trim()} - ${title.trim()} (Lyrics) ft. ${feats[0].trim()}=${artist.trim()} & ${feats[0].trim()} - ${title.trim()} (Lyrics)=${artist.trim()}, ${feats[0].trim()} - ${title.trim()} (Lyrics)=${artist.trim()} - ${title.trim()} [Lyrics] ft. ${feats[0].trim()}=${artist.trim()} & ${feats[0].trim()} - ${title.trim()} [Lyrics]=${artist.trim()}, ${feats[0].trim()} - ${title.trim()} [Lyrics]`;
     case 2:
       `${artist.trim()}, ${feats[0].trim()} - ${title.trim()} (Lyrics) ft. ${feats[1].trim()}=${artist.trim()} - ${title.trim()} (Lyrics) ft. ${feats[0].trim()}, ${feats[1].trim()}=${artist.trim()} - ${title.trim()} (Lyrics) ft. ${feats[0].trim()} & ${feats[1].trim()}=${artist.trim()}, ${feats[0].trim()} - ${title.trim()} [Lyrics] ft. ${feats[1].trim()}=${artist.trim()} - ${title.trim()} [Lyrics] ft. ${feats[0].trim()}, ${feats[1].trim()}=${artist.trim()} - ${title.trim()} [Lyrics] ft. ${feats[0].trim()} & ${feats[1].trim()}`;
     case 3:
-      `${artist.trim()}, ${feats[0]} - ${title.trim()} (Lyrics) ft. ${
-        feats[1]
-      }, ${feats[2]}=${artist.trim()} - ${title.trim()} (Lyrics) ft. ${
-        feats[0]
-      }, ${feats[1]} & ${feats[2]}=${artist.trim()}, ${
-        feats[0]
-      } - ${title.trim()} (Lyrics) ft. ${feats[1]} & ${
+      `${artist.trim()}, ${feats[0]} - ${title.trim()} (Lyrics) ft. ${feats[1]}, ${
         feats[2]
-      }=${artist.trim()}, ${feats[0]} - ${title.trim()} [Lyrics] ft. ${
-        feats[1]
-      }, ${feats[2]}=${artist.trim()} - ${title.trim()} [Lyrics] ft. ${
+      }=${artist.trim()} - ${title.trim()} (Lyrics) ft. ${feats[0]}, ${feats[1]} & ${feats[2]}=${artist.trim()}, ${
         feats[0]
-      }, ${feats[1]} & ${feats[2]}=${artist.trim()}, ${
+      } - ${title.trim()} (Lyrics) ft. ${feats[1]} & ${feats[2]}=${artist.trim()}, ${
         feats[0]
-      } - ${title.trim()} [Lyrics] ft. ${feats[1]} & ${feats[2]}`;
+      } - ${title.trim()} [Lyrics] ft. ${feats[1]}, ${feats[2]}=${artist.trim()} - ${title.trim()} [Lyrics] ft. ${
+        feats[0]
+      }, ${feats[1]} & ${feats[2]}=${artist.trim()}, ${feats[0]} - ${title.trim()} [Lyrics] ft. ${feats[1]} & ${
+        feats[2]
+      }`;
     default:
-      return `${artist.trim()} - ${title.trim()} (Lyrics)=${artist.trim()} - ${title.trim()} [Lyrics]`;
+      return `${artist.trim()} - ${title.trim()} (Lyrics)=${artist.trim()} - ${title.trim()} [Lyrics]=${artist.trim()} - ${title.trim()}`;
   }
 };
