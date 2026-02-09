946df602f0aa6202f852f4a810acfbbeeb4d0037
Nicholas <contactnicholas81@gmail.com>
2026-02-09 16:08:29 +0000

    trim the channel field cuz it adds an extra space for some reason{

--- src/pages/api/v1/generate.ts
+++ src/pages/api/v1/generate.ts
@@ -439,8 +439,8 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   }
 
   if (channel !== 'none') {
-    removedTags += `,${channel}`.toLowerCase();
-    tags += `,${channel.replaceAll('.', '')}`.toLowerCase();
+    removedTags += `,${channel}`.toLowerCase().trim();
+    tags += `,${channel.replaceAll('.', '')}`.toLowerCase().trim();
   }
 
   tagsToBeRemoved = removeTags(finalTitle, finalArtist, finalFeatures, finalFormat.toLowerCase(), tiktok, tags);
